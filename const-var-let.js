fa814689ee95a212bf24ba712820bab641706002
Manuel Madera <maderamanuel25@gmail.com>
2022-05-06 00:32:12 -0400

    Actualiza

---
+++ const-var-let.js
@@ -0,0 +1,12 @@
+/*No usar var para variables, usar const si no voy a cambiar
+el valor o usar let*/
+
+let nombre = 'Manuel';
+
+if(true){
+    let nombre = 'Magneto';
+    console.log(nombre);
+}
+
+console.log(nombre);
+
---
+++ template-string.js
@@ -0,0 +1,16 @@
+const nombre = 'Deadpool';
+const real = 'Wade Winston';
+
+const normal = nombre + ' ' + real;
+const template = `${ nombre } ${ real }`; //Hacerlo asi
+
+console.log(normal);
+console.log(template);
+
+const html = `
+
+<h1>Hola</h1>
+
+`;
+
+console.log(html);
